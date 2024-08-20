40287919e360af01d2284c5d2f4aed8ea20af089
leo <marstvam@gmail.com>
2024-08-20 13:26:59 +0800

    install tailwindcss

---
+++ src/renderer/src/components/footer.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="">努力做事吧！</main>
+</template>
+
+<style lang="scss" scoped>
+main {
+  color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
--- src/renderer/src/views/Home.vue
+++ src/renderer/src/views/Home.vue
@@ -2,6 +2,7 @@
 import FlipClock from '@renderer/composables/FlipClock'
 import { onMounted } from 'vue'
 import '@renderer/assets/flipClock.scss'
+import FooterVue from '@renderer/components/footer.vue'
 
 onMounted(() => {
   const instance = new FlipClock({
@@ -20,12 +21,14 @@ onMounted(() => {
 <template>
   <main class="drag">
     <div id="hd" class="drag"></div>
+    <FooterVue />
   </main>
 </template>
 
 <style lang="scss">
 :root {
   /* --bgColor: red;
+  --width: 20px;
   --color: black; */
 }
 </style>
