398bc14a9967436041ea0c4741fda4c6a6eddc25
leo <marstvam@gmail.com>
2024-08-28 13:58:40 +0800

    安装element-plus ui 组件库

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -2,6 +2,11 @@
 
 import { createApp } from 'vue'
 import App from './App.vue'
+
+// Element plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 import router from '@renderer/route'
 import '@renderer/assets/global.scss'
 import '@renderer/assets/tailwind.css'
@@ -10,6 +15,7 @@ import { createPinia } from 'pinia'
 const pinia = createPinia()
 const app = createApp(App)
 
+app.use(ElementPlus)
 app.use(pinia)
 app.use(router)
 app.mount('#app')
--- src/renderer/src/views/Config.vue
+++ src/renderer/src/views/Config.vue
@@ -1,7 +1,32 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useConfigStore } from '@renderer/store/useConfigStore'
+const { config } = useConfigStore()
+</script>
 
 <template>
-  <main class="bg-white py-12">config...</main>
+  <main class="p-5 bg-[#34495e] text-white">
+    <h2>時鈡顏色</h2>
+    <div class="card">
+      <div class="block">
+        背景顏色
+        <el-color-picker v-model="config.clock.bgColor" />
+      </div>
+      <div class="block">
+        文字顏色
+        <el-color-picker v-model="config.clock.color" />
+      </div>
+    </div>
+  </main>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+h2 {
+  @apply text-sm opacity-80 font-bold  mb-2;
+}
+.card {
+  @apply flex gap-2;
+}
+.block {
+  @apply flex flex-col text-xs gap-2;
+}
+</style>
