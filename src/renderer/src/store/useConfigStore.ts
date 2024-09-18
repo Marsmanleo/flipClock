239ee69c1a47ae64f7e2c7460fa44a5a16da5ecf
leo <marstvam@gmail.com>
2024-09-18 09:43:32 +0800

    本地持久储存配置项

--- src/renderer/src/store/useConfigStore.ts
+++ src/renderer/src/store/useConfigStore.ts
@@ -1,31 +1,34 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
-export const useConfigStore = defineStore('config', () => {
-  const config = ref({
-    clock: {
-      bgColor: '#fff',
-      color: '#000',
-      type: 'clock' as 'clock' | 'timing',
-      timing: {
-        hour: 0,
-        minute: 8,
-        second: 10
+export const useConfigStore = defineStore(
+  'config',
+  () => {
+    const config = ref({
+      clock: {
+        bgColor: '#fff',
+        color: '#000',
+        type: 'clock' as 'clock' | 'timing',
+        timing: {
+          hour: 0,
+          minute: 8,
+          second: 10
+        }
+      },
+      footer: {
+        bgColor: '#16a085',
+        color: '#fff',
+        content: '努力吧！'
       }
-    },
-    footer: {
-      bgColor: '#16a085',
-      color: '#fff',
-      content: '努力吧！'
+    })
+
+    // function increment() {
+    //   count.value++
+    // }
+
+    return {
+      config
     }
-  })
-
-  // function increment() {
-  //   count.value++
-  // }
-
-  return {
-    config
-    // increment
-  }
-})
+  },
+  { persist: true }
+)
