86b94c881e538fdc20fd8681ff7aa5425121d188
leo <marstvam@gmail.com>
2024-09-18 08:27:12 +0800

    时间滚动效果

--- src/renderer/src/components/footer.vue
+++ src/renderer/src/components/footer.vue
@@ -9,7 +9,7 @@ const { config } = useConfigStore()
     class="drag w-full px-2 text-center mt-2 rounded-md text-white flex justify-between items-center"
     :style="{ backgroundColor: config.footer.bgColor, color: config.footer.color }"
   >
-    <div class="text-sm">
+    <div class="text-sm run">
       {{ config.footer.content }}
     </div>
     <Navbar class="" />
@@ -23,5 +23,19 @@ main {
   display: flex;
   justify-content: center;
   align-items: center; */
+}
+.run {
+  animation: identifier 10s infinite both;
+}
+@keyframes identifier {
+  from {
+    transform: translateX(0);
+  }
+  50% {
+    transform: translateX(calc(280px - 100%));
+  }
+  to {
+    transform: translateX(0);
+  }
 }
 </style>
