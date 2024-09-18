13fb05e5c1d72ffa7b7f006840647e16af505094
leo <marstvam@gmail.com>
2024-09-19 03:49:19 +0800

    app page update

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -2,16 +2,17 @@
 import { Crown } from '@icon-park/vue-next'
 import FooterVue from '@renderer/components/footer.vue'
 import useMouseEvent from '@renderer/composables/useMouseEvent'
+
 const { hd } = useMouseEvent()
 hd()
 </script>
 
 <template>
   <Suspense>
-    <RouterView #default="{ Component }">
-      <div class="">
+    <RouterView v-slot="{ Component }">
+      <div>
         <div class="flex justify-center w-full mb-2 drag">
-          <crown theme="outline" size="35" class="text-yellow-400" />
+          <Crown theme="outline" size="35" class="text-yellow-400" />
         </div>
         <component :is="Component" class="flex flex-col justify-center items-center" />
         <FooterVue />
@@ -19,4 +20,7 @@ hd()
     </RouterView>
   </Suspense>
 </template>
-<style lang="less"></style>
+
+<style lang="less">
+/* 可以添加全局樣式 */
+</style>
